60a1f13e7c198e05a63803c22a2e3f577db601f9
tullock <charles.tullock@dynetics.com>
2013-06-27 10:12:52 -0500

    Modified py2exe script to trim down executable size.

--- src/setup.py
+++ src/setup.py
@@ -9,16 +9,17 @@ import sys; sys.argv.append('py2exe')
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/reverseastar')
 
 py2exe_options = dict(
-                      #optimize=2,
                       ascii=True,  # Exclude encodings
                       excludes=['_ssl', 'pdb', 'unittest', 'inspect',
-                                'pyreadline', 'difflib', 'doctest', 'locale', 
-                                'optparse', 'pickle', 'calendar'],
-                      dll_excludes=['msvcr71.dll'],
-                      #compressed=True,  # Compress library.zip
-                      #includes=['reverseastar/algorithm', 'reverseastar/gui'],
+                                'pyreadline', 'difflib', 'doctest', 'locale', 'optparse', 'pickle', 
+                                'calendar',  '_gtkagg', '_tkagg', 'bsddb', 'curses', 'email', 
+                                'pywin.debugger','pywin.debugger.dbgcon', 'pywin.dialogs', 
+                                'tcl','Tkconstants', 'Tkinter'],                      
+                      dll_excludes=['msvcr71.dll', 'libgdk-win32-2.0-0.dll', 'libgobject-2.0-0.dll', 'tcl84.dll',
+                                'tk84.dll'],
+                      compressed=True,
                       includes=['gui', 'model', 'algorithm'],
-                      #bundle_files = 2, # This tells py2exe to bundle everything
+                      bundle_files=1,
                       #build = {'build_base': '../build'},
                       dist_dir='../dist'
                       )
@@ -26,7 +27,8 @@ py2exe_options = dict(
 setup(name='ReverseAStar',
       version='1.0',
       description='Class project for UAV 803',
-      author='Charles Tullock',
+
+      zipfile = None,
 
       console=['reverseastar/reverseastar.py'],
       options={'py2exe': py2exe_options},
